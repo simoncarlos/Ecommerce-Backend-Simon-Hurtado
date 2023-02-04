ab6536146443f2f4f5f7ce012e2b284390380b06
Carlos Diego Simon Hurtado <diegoestushoi@gmail.com>
2023-02-03 21:37:00 -0300

    Añadido y correccion de errores de routers y controladores de productos

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express"
 
+import cart from "../routers/routerCart.js";
+import products from "../routers/routerProducts.js"
+import { errorHandler } from "../middlewares/errorHanding.js";
+
 const app = express()
 
-// middlewares
+// Middlewares
 
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use( express.json() )
+app.use( express.urlencoded({ extended: true }) )
+
+// Routers
+
+app.use("/api/products", products)
+app.use("/api/shoppingcartproducts", cart)
+app.use(errorHandler)
+app.all("*", (req, res) => {
+    res.send( { error: -2, descripcion: `ruta ${req.url}, metodo ${req.method} no implementada` } )
+})
 
 export default app;
